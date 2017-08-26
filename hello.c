32b90d9b269cbf3221337b0c7ccb0fec78a3d35d
Rickosdev <31212501+Rickosdev@users.noreply.github.com>
2017-08-26 19:42:20 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int tesmocule(void);
+
 int main(){
     printf("hello,world!!\n");
+    tesmocule();
+    return 0;
+}
+
+int tesmocule(void){
+    int a;
+    for(a=0;a<10;a++){
+        printf("hello!!\n");
+    }
     return 0;
 }
