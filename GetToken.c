c729b79f9a4dbb28f9f1ea7b1a79c2824da3bae1
Rickosdev <31212501+Rickosdev@users.noreply.github.com>
2017-08-27 22:56:15 +0900

    Update GetToken.c

--- GetToken.c
+++ GetToken.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+void GetToken(FILE* fp,char* buf){
+    static char ch=getc(fp);
+    int i=0;
+    if(ch==EOF)ch=getc(fp);
+    while(ch==' '||ch=='\n')ch=getc(fp);
+    while(ch!=' '&&ch!='\n'&&ch!=EOF){
+        buf[i++]=ch;
+        ch=getc(fp);
+    }
+    return;
+}
